554b820ddf892d1783ed59838fe021c94a32d138
Ninoslav Meic <ninoslav.meic@gmail.com>
2020-12-23 16:30:14 +0100

    Only pg

--- db.py
+++ db.py
@@ -5,6 +5,6 @@ from sqlalchemy.orm import sessionmaker
 
 
 db = declarative_base()
-engine = create_engine(os.environ.get('postgres://bbovcqkgayqdlh:416b864bee156855f21336a8056f97af781eec03180524674f0396ea9f981f28@ec2-54-75-229-28.eu-west-1.compute.amazonaws.com:5432/d5u116ov25cmpr','sqlite:///database2.db'),connect_args={"check_same_thread": False})
+engine = create_engine('DATABASE_URL',connect_args={"check_same_thread": False})
 Session = sessionmaker(bind=engine)
 session = Session()
