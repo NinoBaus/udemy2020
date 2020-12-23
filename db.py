77fa763f4ff4eacd3e496aebb6a3ff5352baf7e8
Ninoslav Meic <ninoslav.meic@gmail.com>
2020-12-23 16:36:39 +0100

    db url

--- db.py
+++ db.py
@@ -2,9 +2,10 @@ import os
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+DATABASE_URL = os.environ['DATABASE_URL']
 
 
 db = declarative_base()
-engine = create_engine('DATABASE_URL',connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL,connect_args={"check_same_thread": False})
 Session = sessionmaker(bind=engine)
 session = Session()
