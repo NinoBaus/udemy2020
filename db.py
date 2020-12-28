7a887d230e949a02e98a8b0041181d477b6f3dd3
Ninoslav Meic <ninoslav.meic@gmail.com>
2020-12-28 21:57:15 +0100

    Restiling

--- db.py
+++ db.py
@@ -3,11 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
-# DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ['DATABASE_URL']
 
 db = declarative_base()
-# engine = create_engine(DATABASE_URL)
-engine = create_engine(os.environ.get('DATABAS_URL','sqlite:///database.db'),connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
+# engine = create_engine(os.environ.get('DATABAS_URL','sqlite:///database.db'),connect_args={"check_same_thread": False})
 
 # session_factory = sessionmaker(bind=engine)
 # Session = scoped_session(session_factory)
